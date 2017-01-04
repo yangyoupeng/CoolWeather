fdd0e0bb12b932a6ac22a5ba65578913db27cda6
Yang You Peng <10036005461@qq.com>
2017-01-04 10:54:12 +0800

    用开源库litepal创建数据库和表的各项配置

---
+++ app/src/main/java/com/yyp/coolweather/db/City.java
@@ -0,0 +1,48 @@
+package com.yyp.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by yangyoupeng on 2017/1/4.
+ */
+
+public class City extends DataSupport {
+
+    private int id;
+    private String cityName;
+    private int cityCode;
+
+    private int provinceId;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getProvinceName() {
+        return cityName;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+}
---
+++ app/src/main/java/com/yyp/coolweather/db/Province.java
@@ -0,0 +1,34 @@
+package com.yyp.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by yangyoupeng on 2017/1/4.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+}
